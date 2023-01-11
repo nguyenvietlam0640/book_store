cc8700e1fde4b0bbda63482ed0fc75c5e9b98893
LamNg <lamnv0640@gmail.com>
2023-01-11 23:45:29 +0700

    initalize

---
+++ test_2.py
@@ -0,0 +1,20 @@
+
+
+
+def example():
+    return 0
+
+def one():
+    assert example() == 0
+
+
+def two():
+    assert example() == 0
+
+
+def three():
+    assert example() == 0
+
+
+def four():
+    assert example() == 0
