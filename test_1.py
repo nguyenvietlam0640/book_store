4df6f6ee48be09ae4ead9bf3c1bef39ba4e410be
LamNg <lamnv0640@gmail.com>
2023-01-12 18:20:58 +0700

    create files for circleci and coveralls

---
+++ test_1.py
@@ -0,0 +1,20 @@
+
+
+
+def example():
+    return 0
+
+def test_one():
+    assert example() == 0
+
+
+def test_two():
+    assert example() == 0
+
+
+def test_three():
+    assert example() == 0
+
+
+def test_four():
+    assert example() == 0
---
+++ test_2.py
@@ -0,0 +1,23 @@
+
+
+def example():
+    return 0
+
+def example_2():
+    return 2
+
+
+def test_one():
+    assert example() == 0
+
+
+def test_two():
+    assert example() == 0
+
+
+def test_three():
+    assert example() == 0
+
+
+def test_four():
+    assert example() == 0
