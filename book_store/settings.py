bd68b754bd507525f1f84aa9640e61fc96221fd8
LamNg <lamnv0640@gmail.com>
2023-01-13 00:15:39 +0700

    initialize register app for book store

--- book_store/settings.py
+++ book_store/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'book_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,3 +123,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
