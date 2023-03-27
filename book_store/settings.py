c577dd91b348efd0e90887d67195f41b1b2a528b
LamNg <lamnv0640@gmail.com>
2023-03-28 00:45:26 +0700

    fix settings.py

--- book_store/settings.py
+++ book_store/settings.py
@@ -16,6 +16,7 @@ import django_heroku
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
