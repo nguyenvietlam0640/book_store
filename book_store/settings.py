ad5686f53b7bda9214c75c830932ff3f6910a20f
LamNg <lamnv0640@gmail.com>
2023-03-30 01:16:12 +0700

    fix settings

--- book_store/settings.py
+++ book_store/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = "/var/lamnv-book-store.herokuapp.com/static/"
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
