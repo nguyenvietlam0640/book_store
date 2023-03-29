25cae1c80b95a75f55157268f2cae98009fc1a07
LamNg <lamnv0640@gmail.com>
2023-03-30 00:19:41 +0700

    update media

--- book_store/settings.py
+++ book_store/settings.py
@@ -156,7 +156,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 CORS_ORIGIN_ALLOW_ALL: True
