dfbeaf43e184f7fdb82326a6f3bb612759ec7ad8
LamNg <lamnv0640@gmail.com>
2023-03-30 01:01:58 +0700

    fix settings

--- book_store/settings.py
+++ book_store/settings.py
@@ -154,7 +154,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = "/var/www/lamnv-bookstore.netlify.app/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
