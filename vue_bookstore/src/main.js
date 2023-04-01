f0eedee240cf8c58b8fb042735e33a03fb286bea
LamNg <lamnv0640@gmail.com>
2023-04-01 17:43:59 +0700

    update vue_bookstore

--- vue_bookstore/src/main.js
+++ vue_bookstore/src/main.js
@@ -5,7 +5,7 @@ import store from './store'
 import axios from 'axios'
 import "./assets/css/style.css"
 
-axios.defaults.baseURL= 'http://127.0.0.1:8000/'
+axios.defaults.baseURL= 'https://lamnv-book-store.herokuapp.com'
 axios.defaults.headers.common['Authorization'] = 'jwt' + localStorage.getItem('jwt')
 
 createApp(App).use(store).use(router, axios).mount('#app')
